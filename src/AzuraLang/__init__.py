c1c27e19ad2736702dfd7179cfaea60c80575aa2
zhan2os1ks <zhanikosan@gmail.com>
2026-06-30 22:12:52 +0200

    BETA version publish

--- src/AzuraLang/__init__.py
+++ src/AzuraLang/__init__.py
@@ -1,4 +1,4 @@
-from .main import window, label, button, guiInput, run, reporterror, windows, useDarkMode
+from .main import window, label, button, guiInput, run, windows, useDarkMode
 
 __version__ = 'BETA-1'
 __author__ = 'Zhan2os1ks'
