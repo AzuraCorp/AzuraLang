a25a85d7fa3462a288d60658a65f6f01f125673e
zhan2os1ks <zhanikosan@gmail.com>
2026-06-30 21:08:15 +0200

    Cleaning the structure

--- a.py
+++
@@ -1,5 +0,0 @@
-from AzuraLang import *
-window("root")
-button("root", name="button1", text="I'm clickable", onclick=lambda:print("Hello!"))
-label("root", pos=lambda:beforeWidget.name("button1"))
-run()
