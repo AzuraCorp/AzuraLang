c02f2967549e8677843b71b4ad5f2f8aa740b3fb
zhan2os1ks <zhanikosan@gmail.com>
2026-09-08 21:07:57 +0200

    Making tests for the apps

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-python ./tests/printart.py
+python printart.py
 echo -e "This is the AzuraLang test script. \nThis script will run all test*.py scripts and try to test all the components."
 echo -e "After every single test you'll be asked questions and you need to answer them."
 echo -e "After everything, a new file will appear in your working directrory called \"test-report.txt\"."
-echo -e "You need to make an issue at https://github.com/AzuraCorp/issues with this file attached to. "
+echo -e "You're help is important to us. If you want to help us,\n make an issue at https://github.com/AzuraCorp/issues with this file attached to. "
 echo -e "(OPTIONAL, BUT RECOMMENDED) Add a fastfetch/neofetch output for better issue fixing."
 read -r -p "Do you want to continue? (y/n)" a
 case $a in
